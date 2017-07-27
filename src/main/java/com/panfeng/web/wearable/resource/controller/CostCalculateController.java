494b6d0c109bba86c8f497bf52307291f2480cca
lirui0 <rui000x@foxmail.com>
2017-07-27 15:06:12 +0800

    l

--- src/main/java/com/panfeng/web/wearable/resource/controller/CostCalculateController.java
+++ src/main/java/com/panfeng/web/wearable/resource/controller/CostCalculateController.java
@@ -82,12 +82,14 @@ public class CostCalculateController extends BaseController {
 			indent.setIndent_tele(telephone == null ? calculate.getPhone() : telephone);
 			String sessionType = info.getSessionType();
 			String indent_recomment = indent.getIndent_recomment();
+			//供应商和内部员工不新建订单
 			switch (sessionType) {
 			case PmsConstant.ROLE_PROVIDER:
 				indent_recomment = "供应商  " + indent_recomment;
-				break;
+				return map;
 			case PmsConstant.ROLE_EMPLOYEE:
 				indent_recomment = "内部员工   "+ indent_recomment;
+				return map;
 			}
 			indent.setIndent_recomment(indent_recomment);
 		}
