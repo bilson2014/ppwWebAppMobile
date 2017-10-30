55ffe89769e3a41b7f563fa34e95ca28c9fc56fa
chengxx <chengxiaoxuan110@163.com>
2017-10-30 10:29:03 +0800

    1

--- src/main/webapp/resources/js/salesman/planOrder.js
+++ src/main/webapp/resources/js/salesman/planOrder.js
@@ -1,18 +1,15 @@
 var InterValObj; // timer变量，控制时间  
 var count = 60; // 间隔函数，1秒执行  
 var sendCodeFlag = true;
-$().ready(function() {	
+$().ready(function() {
 	var localsrc=window.location.href;
 	init();
 	setHeight();
 	initShareNew();
 	success();	
 	var screenWidth = document.documentElement.clientWidth;
-	var screenHeight = document.documentElement.clientHeight;
 	$('body').css('width',screenWidth); 
-	/*$('body').css('height',screenHeight); */
 });
-
 
 function varphone(){
 	var phone=$('#phone').val();
@@ -61,9 +58,9 @@ function success(){
 					productId:-1,
 					teamId:-1,
 					serviceId:-1,
-					phoneCode:$('#num').val(),
-					target:'Pbusinesscard',
-					indentSource : 2//订单来源编号			
+					phoneCode : $('#num').val(),
+					indentSource : 2,//订单来源编号
+					target:'Pbusinesscard'
 				  });	
 			}			 
 		}				 
@@ -80,8 +77,7 @@ function success(){
 	})
 	//报名成功弹框确认事件
 	$('#checkSuccess').on('click',function(){
-		$('#orderSuccess').attr('style','display:none;');
-		
+		$('#orderSuccess').attr('style','display:none;');		
 	})
 }
 //AJAX POST
@@ -130,13 +126,10 @@ function verification(phone,ID){
 			$('#'+ID).text('重新获取');
 			$('#'+ID).removeAttr('disabled');
 		}			
-	}, getContextPath() + '/login/verification/' + phone, null);		
-		
+	}, getContextPath() + '/login/verification/' + phone, null);				
 }
-
-
-function setHeight(){
-	  var screenWidth = document.documentElement.clientWidth;
+function setHeight(){   
+      var screenWidth = document.documentElement.clientWidth;
       var setHeight= screenWidth/16*9;
 	  $('#playVideo').css('height',setHeight);
 	  var imgH = setHeight+15;
@@ -144,13 +137,6 @@ function setHeight(){
 }
 
 function init() {
-	    document.addEventListener("WeixinJSBridgeReady", function () {
-	    	document.getElementById('toPlayVideo').play();
-	     }, false);
-	    document.getElementById('toPlayVideo').play();
-		$('#toPlayVideo').off('click').on('click',function(){
-			document.getElementById('toPlayVideo').play();
-		});
        var swiperV = new Swiper('.swiperVertical', {
 	        direction: 'vertical',
 	        pagination: '.swiper-pagination-v',
@@ -158,33 +144,12 @@ function init() {
 	       // loop:true,
 	        onSlideNextEnd: function(swiper){
 	        	var index = swiper.activeIndex;
-	        	 if(index == 0){
-	        		 $('.spIcon').show();
-	        	 }
-	            if(index == 1){
-	            	document.addEventListener("WeixinJSBridgeReady", function () {
-	     		    	document.getElementById('toPlayFullVideo').play();
-	     		     }, false);
-	     		    document.getElementById('toPlayFullVideo').play();
-	     			$('#toPlayFullVideo').off('click').on('click',function(){
-	     				document.getElementById('toPlayFullVideo').play();
-	     			});
-	            	$('.serWord').removeClass('animation');
-	            	document.getElementById('toPlayVideo').pause();
-	            }
-	            if(index != 1){
-	            	document.getElementById('toPlayFullVideo').pause();
-	            }
-	            if(index == 1){
-	            	$('.spIcon').hide();
-	            }
+	        	
 	           }
 	    });
 }
-	
 
 function initShareNew(){
-	
 	wx.config({
         debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: 'wxb5f2540cff5*****', // 必填，公众号的唯一标识
@@ -195,10 +160,10 @@ function initShareNew(){
     });
     wx.ready(function(){
         wx.onMenuShareAppMessage({
-        	title: '9999元拍企业视频名片', // 分享标题
-            desc: '精准覆盖10000+用户', // 分享描述
-            link: 'http://m.apaipian.com/activity/Pbusinesscard', // 分享链接
-            imgUrl: 'http://m.apaipian.com/resources/images/employee/em1.png', // 分享图标
+        	title: '拍片网-企业视频名片', // 分享标题
+            desc: '', // 分享描述
+            link: 'http://m.apaipian.com/activity/order', // 分享链接
+            imgUrl: 'http://m.apaipian.com/resources/images/salesMan/flyPlay/share.png', // 分享图标
             type: 'link', // 分享类型,music、video或link，不填默认为link
             dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
         /*    success: function () { 
@@ -209,10 +174,10 @@ function initShareNew(){
     /*  wx.error(function(res){
         });*/
         wx.onMenuShareTimeline({
-            title: '9999元拍企业视频名片', // 分享标题
-            desc: '精准覆盖10000+用户', // 分享描述
-            link: 'http://m.apaipian.com/activity/Pbusinesscard', // 分享链接
-            imgUrl: 'http://m.apaipian.com/resources/images/employee/em1.png', // 分享图标
+            title: '拍片网-企业视频名片', // 分享标题
+            desc: '', // 分享描述
+            link: 'http://m.apaipian.com/activity/order', // 分享链接
+            imgUrl: 'http://m.apaipian.com/resources/images/salesMan/flyPlay/share.png', // 分享图标
             type: 'link', // 分享类型,music、video或link，不填默认为link
             dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
         });
