650ca71a7d1e875841c82793ddb00869dd1d278e
chengxx <chengxiaoxuan110@163.com>
2018-03-15 11:09:59 +0800

    xx\

---
+++ src/main/webapp/resources/salesman/special/js/index.js
@@ -0,0 +1,24 @@
+$().ready(function() {
+	
+	var mySwiper = new Swiper('.first .swiper-container', {
+		autoplay: true,//可选选项，自动滑动
+	})
+	
+	 var swiper = new Swiper('.hots .swiper-container', {
+		/* slidesPerView: 'auto',
+	      spaceBetween: 30,
+	      pagination: {
+	        el: '.swiper-pagination',
+	        clickable: true,
+	      },*/
+		  slidesPerView: 1,
+	      centeredSlides: true,
+	      spaceBetween: 30,
+	      grabCursor: true,
+	      pagination: {
+	        el: '.swiper-pagination',
+	        clickable: true,
+	      },
+	    });
+});
+
