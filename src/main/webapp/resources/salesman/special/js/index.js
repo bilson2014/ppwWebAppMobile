e2868f87789447beddedebe4d400dc579e88e539
chengxx <chengxiaoxuan110@163.com>
2018-03-15 18:18:12 +0800

    1

--- src/main/webapp/resources/salesman/special/js/index.js
+++ src/main/webapp/resources/salesman/special/js/index.js
@@ -4,21 +4,34 @@ $().ready(function() {
 		autoplay: true,//可选选项，自动滑动
 	})
 	
-	 var swiper = new Swiper('.hots .swiper-container', {
-		/* slidesPerView: 'auto',
+	/* var swiper = new Swiper('.hots .swiper-container', {
+		 slidesPerView: 'auto',
 	      spaceBetween: 30,
-	      pagination: {
-	        el: '.swiper-pagination',
-	        clickable: true,
-	      },*/
-		  slidesPerView: 1,
-	      centeredSlides: true,
-	      spaceBetween: 30,
-	      grabCursor: true,
 	      pagination: {
 	        el: '.swiper-pagination',
 	        clickable: true,
 	      },
+		  slidesPerView: 1,
+	      centeredSlides: true,
+	      spaceBetween: 30,
+	      grabCursor: true,
+	      
+	    
+	      
+	      pagination: {
+	        el: '.swiper-pagination',
+	        clickable: true,
+	      },
+	    });*/
+	
+	
+	 var swiper = new Swiper('.hots .swiper-container', {
+	        pagination: '.swiper-pagination',
+	        slidesPerView: 'auto',
+	        paginationClickable: true,
+	        spaceBetween:0,
+	        
+	        
 	    });
 });
 
